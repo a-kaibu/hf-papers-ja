8307fa38d6c9fc6c4a0c040031ef86ddd1f39f29
kota <52403688+yashikota@users.noreply.github.com>
2026-05-01 15:54:39 +0900

    main.ts の更新

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const PAPERS_JSON_URL = "https://hf-papers-ja.yashikota.com/data/papers.json";
+const PAPERS_JSON_URL = "https://a-kaibu.github.io/hf-papers-ja/data/papers.json";
 
 type Paper = {
   id: string;
